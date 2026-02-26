28bb30bde03c55b03fa696d38a2991971333880f
ElizarovGA <elizarov.g.a@gmail.com>
2026-02-26 20:51:54 +0300

    Домашнее задание - Основы

--- 4-navigator/app.js
+++ 4-navigator/app.js
@@ -3,8 +3,9 @@ const addressLong = 15;
 const positionLat = 27;
 const positionLong = 20;
 
-const distance = Number(
-  Math.sqrt((positionLat - addressLat) ** 2 + (positionLong - addressLong) ** 2)
-);
+const differenceLat = positionLat - addressLat;
+const differenceLong = positionLong - addressLong;
+
+const distance = Math.sqrt(differenceLat ** 2 + differenceLong ** 2);
 
 console.log(distance);
